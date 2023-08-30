8d7c8e1311bdf401f4acc0d06f72c3f4c2c25f77
Kevin Johnson <john2003.kjj@gmail.com>
2023-08-29 22:14:42 -0500

    minor

--- application.py
+++ application.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request
 from helper import *
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     projectList = readProjects()
     about = getAboutMe()
     return render_template('index.html', about = about,projects = projectList)
 
-@app.route("/Projects/<path:url>")
+@application.route("/Projects/<path:url>")
 def serveProjects(url):
     return render_template("project.html",project = readProject(url))
 
-@app.route("/contact")
+@application.route("/contact")
 def serveContact():
     return render_template("contact.html")
 
-@app.route("/contactSubmit",methods=["POST"])
+@application.route("/contactSubmit",methods=["POST"])
 def getContact():
     data = request.json
     with open("info.csv", 'a') as file:
@@ -26,4 +26,4 @@ def getContact():
 
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
